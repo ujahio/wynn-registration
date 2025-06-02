cde64663f813211f267fa38bc86c6cdcd6acd5cf
Joshua Abu <joshujah007@gmail.com>
2025-06-02 23:38:16 +0400

    install jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	preset: "ts-jest",
+	testEnvironment: "node",
+	moduleFileExtensions: ["ts", "js", "json", "node"],
+	testPathIgnorePatterns: ["/node_modules/", "/.next/", "tests/"],
+	transform: {
+		"^.+\\.(ts|tsx)$": [
+			"ts-jest",
+			{
+				isolatedModules: true,
+			},
+		],
+	},
+	moduleNameMapper: {
+		"^@/(.*)$": "<rootDir>/$1",
+	},
+	testMatch: ["**/lib/**/*.test.ts"],
+};
